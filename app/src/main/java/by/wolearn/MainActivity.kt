77ff7ca40cc810b422ca413a295d5e2568ae79b2
Liza Yakimovich <123yak12@gmail.com>
2019-11-07 15:45:01 +0300

    Clear proj

--- app/src/main/java/by/wolearn/MainActivity.kt
+++ app/src/main/java/by/wolearn/MainActivity.kt
@@ -2,12 +2,23 @@ package by.wolearn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import by.wolearn.core.utils.hide
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        findNavController(R.id.navHostFragment).addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.loginFragmentDestination -> toolbar.hide()
+            }
+        }
     }
 
 }
