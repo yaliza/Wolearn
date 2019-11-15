bab038a5add200a665adc8bc31a2cf01f5cead1b
yaliza <123yak12@gmail.com>
2019-11-15 22:23:35 +0300

    Add action buttons to word card

--- app/src/main/java/by/wolearn/core/utils/ViewUtils.kt
+++ app/src/main/java/by/wolearn/core/utils/ViewUtils.kt
@@ -1,6 +1,8 @@
 package by.wolearn.core.utils
 
 import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 
 fun View.hide() {
@@ -9,4 +11,8 @@ fun View.hide() {
 
 fun View.show() {
     visibility = View.VISIBLE
+}
+
+fun ImageView.load(id: Int) {
+    Glide.with(this).load(id).into(this)
 }
---
+++ app/src/main/java/by/wolearn/core/view/ActionButton.kt
@@ -0,0 +1,32 @@
+package by.wolearn.core.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.cardview.widget.CardView
+import by.wolearn.R
+import by.wolearn.core.utils.load
+import kotlinx.android.synthetic.main.view_action_button.view.*
+
+
+const val DEFAULT_IMAGE = R.drawable.ic_eye
+
+class ActionButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : CardView(context, attrs, defStyleAttr) {
+
+    init {
+        View.inflate(context, R.layout.view_action_button, this)
+        initAttributes(attrs)
+    }
+
+    private fun initAttributes(attrs: AttributeSet?) =
+        context.theme.obtainStyledAttributes(attrs, R.styleable.ActionButton, 0, 0).apply {
+            actionButtonImage.load(
+                getResourceId(R.styleable.ActionButton_action_image, DEFAULT_IMAGE)
+            )
+            recycle()
+        }
+}
