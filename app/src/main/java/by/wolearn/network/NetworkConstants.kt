a7246b3e407162ff2e95c932dfd7fcc95ecd761f
yaliza <123yak12@gmail.com>
2019-12-06 14:53:30 +0300

    Bugfix - clear endpoints

--- app/src/main/java/by/wolearn/network/NetworkConstants.kt
+++ app/src/main/java/by/wolearn/network/NetworkConstants.kt
@@ -3,7 +3,7 @@ package by.wolearn.network
 
 const val BASE_URL = "https://wolearn-api.herokuapp.com/api/v1/"
 const val CATEGORIES = "categories"
-const val WORDS = "categories"
+const val WORDS = "words"
 const val LOGIN = "login"
 const val REGISTER = "register"
 
